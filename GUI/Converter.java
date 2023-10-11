f330e55eda276478b3913f32ae9c08a24652864e
Manchester-United-in-my-heart <129467586+BuiDuongKham@users.noreply.github.com>
2023-10-12 05:49:54 +0700

    add GUI

---
+++ GUI/Converter.java
@@ -0,0 +1,15 @@
+package GUI;
+
+public class Converter extends javax.swing.JFrame {
+    public Converter()
+    {
+
+    }
+    private void initComponents()
+    {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
---
+++ MapAndVariant/DebuggerTest.java
@@ -0,0 +1,10 @@
+package MapAndVariant;
+
+public class DebuggerTest {
+    public static void main(String[] args) {
+        int a = 10;
+        ++a;
+        ++a;
+        --a;
+    }
+}
